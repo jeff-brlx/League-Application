ca48920df3e2eb4b2acc46412416f9adbc31daab
jeff-brlx <jeffreyburluraux@gmail.com>
2024-06-06 17:23:50 +0200

    Initial commit

---
+++ src/test/java/com/supinfo/League_Application/LeagueApplicationTests.java
@@ -0,0 +1,13 @@
+package com.supinfo.League_Application;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeagueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
